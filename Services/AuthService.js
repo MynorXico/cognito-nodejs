3fa049dbd1a96652464d85204dafffa7c9556e5b
Mynor Xico <mxico@pacifiko.com>
2020-06-13 22:51:03 -0600

    Token Validation

--- Services/AuthService.js
+++ Services/AuthService.js
@@ -8,7 +8,7 @@ global.navigator = () => null;
 const AmazonCognitoIdentity = require('amazon-cognito-identify-js');
 const { request } = require('../app');
 const poolData = {
-    UserPoolId: process.env.COGNITO_POOL_ID ,
+    UserPoolId: process.env.COGNITO_POOL_ID,
     ClientId: process.env.COGNITO_CLIENT_ID
 };
 
@@ -26,8 +26,8 @@ exports.Register = function (body, callback) {
         Name: "email", Value: email
     }));
 
-    userPool.signUp(name, password, attributeList, null, function(err, result){
-        if(err)
+    userPool.signUp(name, password, attributeList, null, function (err, result) {
+        if (err)
             callback(err);
 
         var cognitoUser = result.user;
@@ -35,7 +35,7 @@ exports.Register = function (body, callback) {
     });
 }
 
-exports.Login = function(body, callback){
+exports.Login = function (body, callback) {
     var userName = body.name;
     var password = body.password;
     var authenticationDetails = new AmazonCognitoIdentity.authenticationDetails({
@@ -51,12 +51,59 @@ exports.Login = function(body, callback){
     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
 
     cognitoUser.authenticateUser(authenticationDetails, {
-        onSuccess: function(result){
+        onSuccess: function (result) {
             var accesstoken = result.getAccessToken().getJwtToken();
             callback(null, accesstoken);
         },
-        onFailure: (function(err) {
+        onFailure: (function (err) {
             callback(err);
         })
     })
+}
+
+exports.Validate = function (token, callback) {
+    request({
+        url: `https://cognitoidp.${pool_region}.amazonaws.com/${poolData.UserPoolId}/.well-known/jwks.json`,
+        json: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            pems = {};
+            var keys = body['keys'];
+            for (var i = 0; i < keys.length; i++) {
+                var key_id = keys[i].kid;
+                var modulus = keys[i].n;
+                var exponent = keys[i].e;
+                var key_type = keys[i].kty;
+                var jwk = { kty: key_type, n: modulus, e: exponent };
+                var pem = jwkToPem(jwk);
+                pems[key_id] = pem;
+            }
+
+            var decodedJwt = jwt.decode(token, { complete: true });
+            if (!decodedJwt) {
+                console.log("Not a valida JWT token");
+                callback(new Error('Not a valida JWT token'));
+            }
+
+            var kid = decodedJwt.header.kid;
+            var pem = pems[kid];
+            if (!pem) {
+                console.log("Invalid token");
+                callback(new Error('Invalid token'));
+            }
+
+            jwt.verify(token, pem, function (err, payload) {
+                if (err) {
+                    console.log("Invalid Token.");
+                    callback(new Error('Invalid token'));
+                } else {
+                    console.log("Valid Token. ");
+                    callback(null, "Valid token");
+                }
+            })
+        }else{
+            console.log("Error! Unable to download JKWs");
+            callback(error);
+        }
+    });
 }
